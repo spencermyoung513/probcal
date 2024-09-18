db767e72a895743d67a451fb074701d8d5ea3541
Porter Jenkins <porterjenkins@gmail.com>
2024-09-18 15:05:10 -0600

    add RV builder

--- probcal/random_variables/__init__.py
+++ probcal/random_variables/__init__.py
@@ -1,1 +1,8 @@
+from scipy.stats import norm
 from .double_poisson import DoublePoisson
+from ..enums import HeadType
+
+RVS = {
+    HeadType.DOUBLE_POISSON.value: DoublePoisson,
+    HeadType.GAUSSIAN.value: norm,
+}
