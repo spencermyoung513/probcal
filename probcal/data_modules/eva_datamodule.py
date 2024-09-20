1f1146c627b8f568942d2f6484e72fb896040998
rsinema <rileysinema@gmail.com>
2024-09-20 11:27:22 -0600

    added super().__init__() to datamodule

--- probcal/data_modules/eva_datamodule.py
+++ probcal/data_modules/eva_datamodule.py
@@ -28,15 +28,15 @@ class EVADataModule(L.LightningDataModule):
         persistent_workers: bool,
         surface_image_path: bool = False,
     ):
+        super().__init__()
         self.root_dir = root_dir
         self.batch_size = batch_size
         self.num_workers = num_workers
         self.persistent_workers = persistent_workers
         self.surface_image_path = surface_image_path
-        self.prepare_data()
 
     def prepare_data(self) -> None:
-        # prepare/extract the images from the zip files
+        # download and extract the data
         EVADataset(self.root_dir)
 
     def setup(self, stage):
