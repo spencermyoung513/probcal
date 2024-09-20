4a4e4f01c558d9dc02c61ebcaa99725ec4c286f4
Porter Jenkins <porterjenkins@gmail.com>
2024-09-19 20:56:21 -0600

    removing samplers builder in favor of more elegant _sample_impl() method()

--- probcal/samplers/__init__.py
+++ probcal/samplers/__init__.py
@@ -1,10 +1,3 @@
 from .gaussian_sampler import GaussianSampler
 from .poisson_sampler import PoissonSampler
 from .nbinom_sampler import NegBinomSampler
-from ..enums import HeadType
-
-SAMPLERS = {
-    HeadType.GAUSSIAN.value: GaussianSampler,
-    HeadType.POISSON.value: PoissonSampler,
-    HeadType.NEGATIVE_BINOMIAL.value: NegBinomSampler
-}
