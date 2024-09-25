596cd732751f7b13525460493a80b042b33debd7
rsinema <rileysinema@gmail.com>
2024-09-25 14:23:08 -0600

    changed data path in eva_eval script

--- eva_eval_script.py
+++ eva_eval_script.py
@@ -21,6 +21,8 @@ evaluator = CalibrationEvaluator(settings)
 model = GaussianNN.load_from_checkpoint("chkp/eva_gaussian/version_0/best_mae.ckpt")
 
 # You can use any lightning data module (preferably, the one with the dataset the model was trained on).
-data_module = EVADataModule(root_dir="data", batch_size=4, num_workers=0, persistent_workers=False)
+data_module = EVADataModule(
+    root_dir="data/eva", batch_size=4, num_workers=0, persistent_workers=False
+)
 calibration_results = evaluator(model=model, data_module=data_module)
 calibration_results.save("eva_gaussian_calibration_results.npz")
