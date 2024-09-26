1a50330ed3ac0ded548885426fdd8f146f1ab372
rsinema <rileysinema@gmail.com>
2024-09-26 14:05:43 -0600

    still can't spell gaussian correct

--- probcal/models/__init__.py
+++ probcal/models/__init__.py
@@ -1,5 +1,6 @@
 from .double_poisson_nn import DoublePoissonNN
 from .faithful_gaussian_nn import FaithfulGaussianNN
 from .gaussian_nn import GaussianNN
+from .natural_gaussion_nn import NaturalGaussianNN
 from .neg_binom_nn import NegBinomNN
 from .poisson_nn import PoissonNN
