57f83018528ae7ffa213bc351dd0958e1092b906
rsinema <rileysinema@gmail.com>
2024-09-26 13:04:47 -0600

    added faith gaus import statement

--- probcal/models/__init__.py
+++ probcal/models/__init__.py
@@ -1,4 +1,5 @@
 from .double_poisson_nn import DoublePoissonNN
+from .faithful_gaussian_nn import FaithfulGaussianNN
 from .gaussian_nn import GaussianNN
 from .neg_binom_nn import NegBinomNN
 from .poisson_nn import PoissonNN
