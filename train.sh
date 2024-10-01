797dc50cd41c4df25939e6885ecc5a6e826d21d5
Andrew Hall <andrewmartinhall2@gmail.com>
2024-10-01 13:41:49 -0600

    Removing extra sh file

--- train.sh
+++
@@ -1,3 +0,0 @@
-python -m probcal.training.train_model --config configs/train/aaf/gaussian_beta_cfg.yaml
-python -m probcal.training.train_model --config configs/train/aaf/natural_gaussian_cfg.yaml
-python -m probcal.training.train_model --config configs/train/aaf/faithful_gaussian_cfg.yaml
