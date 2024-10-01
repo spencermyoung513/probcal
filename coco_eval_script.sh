9aa2eacb3a32a277c333d40262b57214cda095a7
rsinema <rileysinema@gmail.com>
2024-10-01 08:47:13 -0600

    commented for testing

--- coco_eval_script.sh
+++ coco_eval_script.sh
@@ -24,67 +24,67 @@ done
 
 echo "All $num_loops MCMD completed."
 
-echo "evaluating immer model..."
+# echo "evaluating immer model..."
 
-for i in $(seq 1 $num_loops)
-do
-    echo "MCMD $i of $num_loops"
-    python probcal/evaluation/eval_model.py --config configs/eval/coco/coco_immer_cfg.yaml
-    python eval_script.py --results logs/coco/immer_results/calibration_results.npz --data logs/coco/immer_results/mcmd_vals.txt
-    echo "Completed MCMD $i"
-    echo "-------------------"
-done
+# for i in $(seq 1 $num_loops)
+# do
+#     echo "MCMD $i of $num_loops"
+#     python probcal/evaluation/eval_model.py --config configs/eval/coco/coco_immer_cfg.yaml
+#     python eval_script.py --results logs/coco/immer_results/calibration_results.npz --data logs/coco/immer_results/mcmd_vals.txt
+#     echo "Completed MCMD $i"
+#     echo "-------------------"
+# done
 
-echo "All $num_loops MCMD completed."
+# echo "All $num_loops MCMD completed."
 
-echo "evaluating negative binomial model..."
+# echo "evaluating negative binomial model..."
 
-for i in $(seq 1 $num_loops)
-do
-    echo "MCMD $i of $num_loops"
-    python probcal/evaluation/eval_model.py --config configs/eval/coco/coco_nbinom_cfg.yaml
-    python eval_script.py --results logs/coco/nbinom_results/calibration_results.npz --data logs/coco/nbinom_results/mcmd_vals.txt
-    echo "Completed MCMD $i"
-    echo "-------------------"
-done
+# for i in $(seq 1 $num_loops)
+# do
+#     echo "MCMD $i of $num_loops"
+#     python probcal/evaluation/eval_model.py --config configs/eval/coco/coco_nbinom_cfg.yaml
+#     python eval_script.py --results logs/coco/nbinom_results/calibration_results.npz --data logs/coco/nbinom_results/mcmd_vals.txt
+#     echo "Completed MCMD $i"
+#     echo "-------------------"
+# done
 
-echo "All $num_loops MCMD completed."
+# echo "All $num_loops MCMD completed."
 
-echo "evaluating poisson model..."
+# echo "evaluating poisson model..."
 
-for i in $(seq 1 $num_loops)
-do
-    echo "MCMD $i of $num_loops"
-    python probcal/evaluation/eval_model.py --config configs/eval/coco/coco_poisson_cfg.yaml
-    python eval_script.py --results logs/coco/poisson_results/calibration_results.npz --data logs/coco/poisson_results/mcmd_vals.txt
-    echo "Completed MCMD $i"
-    echo "-------------------"
-done
+# for i in $(seq 1 $num_loops)
+# do
+#     echo "MCMD $i of $num_loops"
+#     python probcal/evaluation/eval_model.py --config configs/eval/coco/coco_poisson_cfg.yaml
+#     python eval_script.py --results logs/coco/poisson_results/calibration_results.npz --data logs/coco/poisson_results/mcmd_vals.txt
+#     echo "Completed MCMD $i"
+#     echo "-------------------"
+# done
 
-echo "All $num_loops MCMD completed."
+# echo "All $num_loops MCMD completed."
 
-echo "evaluating seitzer model..."
+# echo "evaluating seitzer model..."
 
-for i in $(seq 1 $num_loops)
-do
-    echo "MCMD $i of $num_loops"
-    python probcal/evaluation/eval_model.py --config configs/eval/coco/coco_seitzer_cfg.yaml
-    python eval_script.py --results logs/coco/seitzer_results/calibration_results.npz --data logs/coco/seitzer_results/mcmd_vals.txt
-    echo "Completed MCMD $i"
-    echo "-------------------"
-done
+# for i in $(seq 1 $num_loops)
+# do
+#     echo "MCMD $i of $num_loops"
+#     python probcal/evaluation/eval_model.py --config configs/eval/coco/coco_seitzer_cfg.yaml
+#     python eval_script.py --results logs/coco/seitzer_results/calibration_results.npz --data logs/coco/seitzer_results/mcmd_vals.txt
+#     echo "Completed MCMD $i"
+#     echo "-------------------"
+# done
 
-echo "All $num_loops MCMD completed."
+# echo "All $num_loops MCMD completed."
 
-echo "evaluating stirn model..."
+# echo "evaluating stirn model..."
 
-for i in $(seq 1 $num_loops)
-do
-    echo "MCMD $i of $num_loops"
-    python probcal/evaluation/eval_model.py --config configs/eval/coco/coco_stirn_cfg.yaml
-    python eval_script.py --results logs/coco/stirn_results/calibration_results.npz --data logs/coco/stirn_results/mcmd_vals.txt
-    echo "Completed MCMD $i"
-    echo "-------------------"
-done
+# for i in $(seq 1 $num_loops)
+# do
+#     echo "MCMD $i of $num_loops"
+#     python probcal/evaluation/eval_model.py --config configs/eval/coco/coco_stirn_cfg.yaml
+#     python eval_script.py --results logs/coco/stirn_results/calibration_results.npz --data logs/coco/stirn_results/mcmd_vals.txt
+#     echo "Completed MCMD $i"
+#     echo "-------------------"
+# done
 
-echo "All $num_loops MCMD completed."
+# echo "All $num_loops MCMD completed."
