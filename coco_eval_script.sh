2d9b199ab04bc38523ab905ea08963adb9d72912
Riley Sinema <rileysinema@gmail.com>
2024-10-04 17:43:07 -0600

    eval script

--- coco_eval_script.sh
+++ coco_eval_script.sh
@@ -9,12 +9,12 @@
 # python probcal/evaluation/eval_model.py --config configs/eval/coco/coco_immer_cfg.yaml
 # echo "evaluating poisson model..."
 # python probcal/evaluation/eval_model.py --config configs/eval/coco/coco_poisson_cfg.yaml
-# echo "evaluating seitzer model..."
-# python probcal/evaluation/eval_model.py --config configs/eval/coco/coco_seitzer_cfg.yaml
+echo "evaluating seitzer model..."
+python probcal/evaluation/eval_model.py --config configs/eval/coco/coco_seitzer_cfg.yaml
 # echo "evaluating stirn model..."
 # python probcal/evaluation/eval_model.py --config configs/eval/coco/coco_stirn_cfg.yaml
-echo "evaluating gaussian model..."
-python probcal/evaluation/eval_model.py --config configs/eval/coco/coco_gaussian_cfg.yaml
+# echo "evaluating gaussian model..."
+# python probcal/evaluation/eval_model.py --config configs/eval/coco/coco_gaussian_cfg.yaml
 
 # if [ $# -eq 1 ]; then
 #     # Check if the argument is a positive integer
