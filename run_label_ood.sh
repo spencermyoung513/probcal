c8bc3fca87d20ede01765557713e40108ed2de1d
Porter Jenkins <porterjenkins@gmail.com>
2024-10-06 14:32:35 -0600

    add bash scripts to automate labe and mixup experiments

---
+++ run_label_ood.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+export PYTHONPATH=/Users/porterjenkins/code/probcal
+
+n=5
+m=5
+
+
+echo "OOD Gaussian"
+for ((i=0; i<n; i++))
+do
+  for ((j=0; j<m; j++))
+    do
+    python3 probcal/experiments/ood.py --cfg-path configs/experiments/ood_label_noise_coco_gaussian_${j}.yaml
+    done
+done
+
+
+
+# POISSON
+echo "OOD Poisson"
+for ((i=0; i<n; i++))
+do
+  for ((j=0; j<m; j++))
+    do
+    python3 probcal/experiments/ood.py --cfg-path configs/experiments/ood_label_noise_coco_poisson_${j}.yaml
+    done
+done
+
+
+# DDPN
+echo "OOD DDPN"
+for ((i=0; i<n; i++))
+do
+  for ((j=0; j<m; j++))
+    do
+    python3 probcal/experiments/ood.py --cfg-path configs/experiments/ood_label_noise_coco_ddpn_${j}.yaml
+    done
+done
