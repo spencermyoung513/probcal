f23dc20562bdf5ebe28e1c4cdcb2ad7d93e6df9f
Porter Jenkins <porterjenkins@gmail.com>
2024-10-06 13:33:44 -0600

    fix bounds of for loop in bash script

--- run_blur_ood.sh
+++ run_blur_ood.sh
@@ -5,15 +5,15 @@ n=5
 m=5
 # GAUSSIAN
 echo "No Blur - Gaussian Baseline"
-for ((i=1; i<=n; i++))
+for ((i=0; i<n; i++))
 do
   python3 probcal/experiments/ood.py --cfg-path configs/experiments/coco_gaussian.yaml
 done
 
 echo "OOD Gaussian"
-for ((i=0; i<=n; i++))
+for ((i=0; i<n; i++))
 do
-  for ((j=0; j<=m; j++))
+  for ((j=0; j<m; j++))
     do
     python3 probcal/experiments/ood.py --cfg-path configs/experiments/ood_blur_coco_gaussian_${j}.yaml
     done
@@ -23,15 +23,15 @@ done
 
 # POISSON
 echo "No Blur - Poisson Baseline$"
-for ((i=1; i<=n; i++))
+for ((i=0; i<n; i++))
 do
   python3 probcal/experiments/ood.py --cfg-path configs/experiments/coco_poisson.yaml
 done
 
 echo "OOD Poisson"
-for ((i=0; i<=n; i++))
+for ((i=0; i<n; i++))
 do
-  for ((j=0; j<=m; j++))
+  for ((j=0; j<m; j++))
     do
     python3 probcal/experiments/ood.py --cfg-path configs/experiments/ood_blur_coco_poisson_${j}.yaml
     done
@@ -40,15 +40,15 @@ done
 
 # DDPN
 echo "No Blur - DDPN Baseline"
-for ((i=1; i<=n; i++))
+for ((i=0; i<n; i++))
 do
   python3 probcal/experiments/ood.py --cfg-path configs/experiments/coco_ddpn.yaml
 done
 
 echo "OOD POISSON"
-for ((i=0; i<=n; i++))
+for ((i=0; i<n; i++))
 do
-  for ((j=0; j<=m; j++))
+  for ((j=0; j<m; j++))
     do
     python3 probcal/experiments/ood.py --cfg-path configs/experiments/ood_blur_coco_ddpn_${j}.yaml
     done
