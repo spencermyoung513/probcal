9e97995c6ef9607bee91884aa550f32701e3ea64
Riley Sinema <rileysinema@gmail.com>
2024-10-09 09:09:23 -0600

    stage

--- coco_eval_script.sh
+++ coco_eval_script.sh
@@ -19,8 +19,8 @@
 # cd ../../..
 
 
-# echo "evaluating nbinom model..."
-# python probcal/evaluation/eval_model.py --config configs/eval/coco/coco_nbinom_cfg.yaml
+echo "evaluating nbinom model..."
+python probcal/evaluation/eval_model.py --config configs/eval/coco/coco_nbinom_cfg.yaml
 # echo "evaluating ddpn model..."
 # python probcal/evaluation/eval_model.py --config configs/eval/coco/coco_ddpn_cfg.yaml
 # echo "evaluating immer model..."
@@ -29,7 +29,7 @@
 # python probcal/evaluation/eval_model.py --config configs/eval/coco/coco_poisson_cfg.yaml
 # echo "evaluating seitzer model..."
 # python probcal/evaluation/eval_model.py --config configs/eval/coco/coco_seitzer_cfg.yaml
-echo "evaluating stirn model..."
-python probcal/evaluation/eval_model.py --config configs/eval/coco/coco_stirn_cfg.yaml
+# echo "evaluating stirn model..."
+# python probcal/evaluation/eval_model.py --config configs/eval/coco/coco_stirn_cfg.yaml
 # echo "evaluating gaussian model..."
 # python probcal/evaluation/eval_model.py --config configs/eval/coco/coco_gaussian_cfg.yaml
