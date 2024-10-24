6780fd5fe1edaf596f072ade97eb37dfe2680ce9
cbedgren <coleedgren@gmail.com>
2024-10-24 12:59:00 -0600

    End of day

--- probcal/experiment.py
+++ probcal/experiment.py
@@ -1,11 +1,17 @@
 import math
 import os
+import warnings
+from functools import partial
+from typing import Optional
+from typing import Type
 
 import lightning as L
 import matplotlib.pyplot as plt
 import numpy as np
 import torch
 from lightning.pytorch.loggers import CSVLogger
+from torch import nn
+from torch.utils.data import TensorDataset, DataLoader
 
 from probcal.enums import DatasetType
 from probcal.enums import LRSchedulerType
@@ -16,6 +22,17 @@ from probcal.utils.experiment_utils import fix_random_seed
 from probcal.utils.experiment_utils import get_chkp_callbacks
 from probcal.utils.experiment_utils import get_datamodule
 
+from probcal.evaluation.probabilistic_evaluator import ProbabilisticEvaluator, ProbabilisticEvaluatorSettings
+from probcal.enums import BetaSchedulerType
+from probcal.enums import LRSchedulerType
+from probcal.enums import OptimizerType
+from probcal.evaluation.custom_torchmetrics import AverageNLL
+from probcal.models.backbones import Backbone
+from probcal.models.regression_nn import RegressionNN
+from probcal.training.beta_schedulers import CosineAnnealingBetaScheduler
+from probcal.training.beta_schedulers import LinearBetaScheduler
+from probcal.evaluation.kernels import rbf_kernel
+
 BACKBONE_TYPE = MLP
 BACKBONE_KWARGS = {"input_dim": 1}
 OPTIM_TYPE = OptimizerType.ADAM_W
@@ -25,10 +42,10 @@ LR_SCHEDULER_KWARGS = {"T_max": 200, "eta_min": 0, "last_epoch": -1}
 
 DATASET_PATH = "data/discrete_sine_wave/discrete_sine_wave.npz"
 
-CHKP_DIR = "chkp/gauss_classic"
+CHKP_DIR = "chkp/gauss_reg"
 
 
-def train_model():
+def train_model(ModelClass: RegressionNN):
 
     fix_random_seed(1124)
 
@@ -36,7 +53,7 @@ def train_model():
     batch_size = 32
     datamodule = get_datamodule(dataset_type, DATASET_PATH, batch_size)
 
-    model = GaussianNN(
+    model = ModelClass(
         backbone_type=BACKBONE_TYPE,
         backbone_kwargs=BACKBONE_KWARGS,
         optim_type=OPTIM_TYPE,
@@ -66,9 +83,9 @@ def train_model():
     trainer.fit(model=model, datamodule=datamodule)
 
 
-def generate_figure():
+def generate_figure(ModelClass):
 
-    model = GaussianNN.load_from_checkpoint(
+    model = ModelClass.load_from_checkpoint(
         f"{CHKP_DIR}/last.ckpt",
         backbone_type=BACKBONE_TYPE,
         backbone_kwargs=BACKBONE_KWARGS,
@@ -129,7 +146,138 @@ def generate_figure():
     plt.close()
 
 
+def gaussian_nll_cce(
+    model: GaussianNN,inputs: torch.Tensor, outputs: torch.Tensor, targets: torch.Tensor, beta: float | None = None, lmbda: float = 0.1,
+) -> torch.Tensor:
+
+    if targets.size(1) != 1:
+        warnings.warn(
+            f"Targets tensor for `gaussian_nll` expected to be of shape (n, 1) but got shape {targets.shape}. This may result in unexpected training behavior."
+        )
+    if beta is not None:
+        if beta < 0 or beta > 1:
+            raise ValueError(f"Invalid value of beta specified. Must be in [0, 1]. Got {beta}")
+        
+    sample_dataset = TensorDataset(inputs, outputs)
+    grid_dataset = TensorDataset(inputs, targets)
+
+    sample_loader = DataLoader(sample_dataset, batch_size=64, shuffle=True)
+    grid_loader = DataLoader(grid_dataset, batch_size=64, shuffle=True)
+
+    device = torch.device(
+        "mps"
+        if torch.backends.mps.is_available()
+        else "cuda"
+        if torch.cuda.is_available()
+        else "cpu"
+    )
+
+    x_vals = torch.cat([x for x, _ in sample_loader], dim=0)
+    gamma = (1 / (2 * x_vals.var())).item()
+    cce_input_kernel = partial(rbf_kernel, gamma=gamma)
+
+    prob_eval_settings = ProbabilisticEvaluatorSettings(
+        dataset_type=DatasetType.TABULAR,
+        device=device,
+        cce_num_trials=5,
+        cce_input_kernel=cce_input_kernel,
+        cce_output_kernel="rbf",
+        cce_lambda=.1,
+        cce_num_samples=1,
+        ece_bins=50,
+        ece_weights="frequency",
+        ece_alpha=1.0
+    )
+
+    mu, logvar = torch.split(outputs, [1, 1], dim=-1)
+    nll = 0.5 * (torch.exp(-logvar) * (targets - mu) ** 2 + logvar)
+    evaluator = ProbabilisticEvaluator(prob_eval_settings)
+    cce_vals  = evaluator.compute_cce(
+        model=model,
+        grid_loader=grid_loader,
+        sample_loader=sample_loader
+    )
+    mean_cce = cce_vals.mean().item()
+    
+    losses = nll + lmbda*mean_cce
+
+    if beta is not None and beta != 0:
+        var = torch.exp(logvar)
+        losses = torch.pow(var.detach(), beta) * losses
+
+    return losses.mean()
+
+
+class RegularizedGaussianNN(GaussianNN):
+
+    def __init__(
+        self,
+        backbone_type: Type[Backbone],
+        backbone_kwargs: dict,
+        optim_type: Optional[OptimizerType] = None,
+        optim_kwargs: Optional[dict] = None,
+        lr_scheduler_type: Optional[LRSchedulerType] = None,
+        lr_scheduler_kwargs: Optional[dict] = None,
+        beta_scheduler_type: BetaSchedulerType | None = None,
+        beta_scheduler_kwargs: dict | None = None,
+    ):
+        
+        if beta_scheduler_type == BetaSchedulerType.COSINE_ANNEALING:
+            self.beta_scheduler = CosineAnnealingBetaScheduler(**beta_scheduler_kwargs)
+        elif beta_scheduler_type == BetaSchedulerType.LINEAR:
+            self.beta_scheduler = LinearBetaScheduler(**beta_scheduler_kwargs)
+        else:
+            self.beta_scheduler = None
+
+        super(GaussianNN, self).__init__(
+            loss_fn=partial(
+                gaussian_nll_cce,
+                beta=(
+                    self.beta_scheduler.current_value if self.beta_scheduler is not None else None
+                ),
+            ),
+            backbone_type=backbone_type,
+            backbone_kwargs=backbone_kwargs,
+            optim_type=optim_type,
+            optim_kwargs=optim_kwargs,
+            lr_scheduler_type=lr_scheduler_type,
+            lr_scheduler_kwargs=lr_scheduler_kwargs,
+        )
+        self.head = nn.Linear(self.backbone.output_dim, 2)
+        self.nll = AverageNLL()
+        self.save_hyperparameters()
+
+    def training_step(self, batch: torch.Tensor) -> torch.Tensor:
+        x, y = batch
+        y_hat = self(x)
+        loss = self.loss_fn(model=self, inputs=x.view(-1, 1).float(), outputs=y_hat, targets=y.view(-1, 1).float())
+        self.log("train_loss", loss, prog_bar=True, on_epoch=True)
+
+        with torch.no_grad():
+            point_predictions = self.point_prediction(y_hat, training=True).flatten()
+            self.train_rmse.update(point_predictions, y.flatten().float())
+            self.train_mae.update(point_predictions, y.flatten().float())
+            self.log("train_rmse", self.train_rmse, on_epoch=True)
+            self.log("train_mae", self.train_mae, on_epoch=True)
+            
+        return loss
+    
+    def validation_step(self, batch: torch.Tensor) -> torch.Tensor:
+        x, y = batch
+        y_hat = self(x)
+        loss = self.loss_fn(model=self, inputs=x.view(-1, 1).float(), outputs=y_hat, targets=y.view(-1, 1).float())
+        self.log("val_loss", loss, prog_bar=True, on_epoch=True, sync_dist=True)
+
+        # Since we used the model's forward method, we specify training=True to get the proper transforms.
+        point_predictions = self.point_prediction(y_hat, training=True).flatten()
+        self.val_rmse.update(point_predictions, y.flatten().float())
+        self.val_mae.update(point_predictions, y.flatten().float())
+        self.log("val_rmse", self.val_rmse, on_epoch=True)
+        self.log("val_mae", self.val_mae, on_epoch=True)
+
+        return loss
+
 if __name__ == "__main__":
     if not os.path.exists(f"{CHKP_DIR}/last.ckpt"):
-        train_model()
+        train_model(RegularizedGaussianNN)
     generate_figure()
