1307a2051acf88dbb15b595786675e7d77a06146
rsinema <rileysinema@gmail.com>
2024-11-21 14:11:08 -0700

    added kronecker kernel

--- probcal/evaluation/probabilistic_evaluator.py
+++ probcal/evaluation/probabilistic_evaluator.py
@@ -19,15 +19,17 @@ from torch.utils.data import DataLoader
 from torchvision.transforms import Compose
 from tqdm import tqdm
 
-from probcal.data_modules.mnist_datamodule_rotate import MNISTDataModuleRotate
 from probcal.enums import DatasetType
 from probcal.evaluation.kernels import bhattacharyya_kernel
+from probcal.evaluation.kernels import kronecker_kernel
 from probcal.evaluation.kernels import laplacian_kernel
 from probcal.evaluation.kernels import polynomial_kernel
 from probcal.evaluation.kernels import rbf_kernel
 from probcal.evaluation.metrics import compute_mcmd_torch
 from probcal.evaluation.metrics import compute_regression_ece
 from probcal.models.regression_nn import RegressionNN
+
+# from probcal.data_modules.mnist_datamodule_rotate import MNISTDataModuleRotate
 
 
 @dataclass
@@ -90,7 +92,7 @@ class ProbabilisticEvaluatorSettings:
     cce_use_val_split_for_S: bool = False
     cce_num_trials: int = 5
     cce_input_kernel: Literal["polynomial"] | KernelFunction = "polynomial"
-    cce_output_kernel: Literal["rbf", "laplacian", "bhatt"] | KernelFunction = "rbf"
+    cce_output_kernel: Literal["rbf", "laplacian", "bhatt", "kron"] | KernelFunction = "rbf"
     cce_lambda: float = 0.1
     cce_num_samples: int = 1
     ece_bins: int = 50
@@ -118,10 +120,10 @@ class ProbabilisticEvaluator:
         # val_dataloader = data_module.val_dataloader()
         grid_dataloader = data_module.test_dataloader()
         test_dataloader = data_module.test_dataloader()
-        if isinstance(data_module, MNISTDataModuleRotate):
-            rotation = data_module.test_rotation
-        else:
-            rotation = 0
+        # if isinstance(data_module, MNISTDataModuleRotate):
+        #     rotation = data_module.test_rotation
+        # else:
+        #     rotation = 0
 
         print(f"Running {self.settings.cce_num_trials} CCE computation(s)...")
         cce_results = []
@@ -498,6 +500,8 @@ class ProbabilisticEvaluator:
             y_kernel = partial(laplacian_kernel, gamma=(1 / (2 * y.float().var())).item())
         elif self.settings.cce_output_kernel == "bhatt":
             y_kernel = bhattacharyya_kernel
+        elif self.settings.cce_output_kernel == "kron":
+            y_kernel = kronecker_kernel
 
         return x_kernel, y_kernel
 
