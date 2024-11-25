6db2958a0999c33c1460192e2bb5f9fc0a1675eb
rsinema <rileysinema@gmail.com>
2024-11-25 12:34:54 -0700

    added example print stmt for getting cond of a mat

--- probcal/evaluation/metrics.py
+++ probcal/evaluation/metrics.py
@@ -226,6 +226,7 @@ def compute_mcmd_torch(
     W_X_prime = torch.cholesky_inverse(L_prime)
 
     K_Y = y_kernel(y, y)
+    print(f"Condition number of K_Y: {cond(K_Y)}")
     K_Y_prime = y_kernel(y_prime, y_prime)
     K_Y_Y_prime = y_kernel(y, y_prime)
 
