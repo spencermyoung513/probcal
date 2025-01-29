f7c001bf91ea1a2cd411e9cf6c1ec4dc1d2824a2
rsinema <rileysinema@gmail.com>
2025-01-29 12:32:57 -0700

    changed script message

--- scripts/run_exp.sh
+++ scripts/run_exp.sh
@@ -34,7 +34,7 @@ fi
 
 for model_dir in ${experiment_dir}/*; do
     model_name=$(basename "$model_dir")
-    echo "Running ${experiment_name} ood exp on ${model_name} head trained on ${dataset}..."
+    echo "Running ${experiment_name} ood exp on ${dataset} ${model_name} head"
 
     head_list=()
     for file in ${experiment_dir}/${model_name}/*.yaml; do
@@ -46,5 +46,5 @@ for model_dir in ${experiment_dir}/*; do
         python probcal/experiments/ood.py \
             --config ${experiment_dir}/${model_name}/${head}.yaml
     done
-    echo "${experiment_name} ood exp on ${model_name} head trained on ${dataset} done."
+    echo "${experiment_name} ood exp on ${dataset} ${model_name} head done."
 done
