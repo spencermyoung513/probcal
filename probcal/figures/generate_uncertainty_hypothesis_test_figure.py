002bac80ed056e288c95fd21ea8de5d05ec97d10
Porter Jenkins <porterjenkins@gmail.com>
2025-02-07 12:56:02 -0700

    style

--- probcal/figures/generate_uncertainty_hypothesis_test_figure.py
+++ probcal/figures/generate_uncertainty_hypothesis_test_figure.py
@@ -12,11 +12,23 @@ def _transform_df(df: pd.DataFrame) -> pd.DataFrame:
     return df
 
 
-def _hyp_test(arr1: np.ndarray, arr2: np.ndarray) -> float:
+def _hyp_test(arr1: np.ndarray, arr2: np.ndarray, alternative: str = "two-sided") -> float:
+    """
+    Performs a hypothesis test (t-test) on two arrays of data to determine if there is a significant difference between them.
+
+    Args:
+        arr1 (np.ndarray): The first array of data.
+        arr2 (np.ndarray): The second array of data.
+        alternative (str): Defines the alternative hypothesis. Options are 'two-sided', 'less', or 'greater'.
+
+    Returns:
+        float: The p-value from the t-test, indicating the probability of observing the data given that the null hypothesis is true.
+    """
+
     arr1 = arr1[~np.isnan(arr1)]
     arr2 = arr2[~np.isnan(arr2)]
 
-    _, p_value = ttest_ind(arr1, arr2, equal_var=False, alternative="greater")
+    _, p_value = ttest_ind(arr1, arr2, equal_var=False, alternative=alternative)
     return p_value
 
 
@@ -35,6 +47,43 @@ def main(boostrap_results_path: str, no_boostrap_results_path: str):
 
     df_bs = pd.read_csv(boostrap_results_path)
     df_bs = _transform_df(df_bs)
+
+    p_value_cce_ece = _hyp_test(df_bs["coef_var_cce"].values, df_bs["coef_var_ece"].values)
+    p_value_cce_nll = _hyp_test(df_bs["coef_var_cce"].values, df_bs["coef_var_nll"].values)
+
+    p_value_cce_ece_greater = _hyp_test(
+        df_bs["coef_var_cce"].values, df_bs["coef_var_ece"].values, alternative="greater"
+    )
+
+    _get_kde_plot(
+        df_bs["coef_var_cce"].values,
+        df_bs["coef_var_ece"].values,
+        "CCE",
+        "ECE",
+        r"CCE vs ECE Coeff. of Variation ($\frac{\sigma}{\mu}$) "
+        f"- p value: {p_value_cce_ece:.3f}",
+        "./artifacts/cce_ece_kde.pdf",
+    )
+
+    _get_kde_plot(
+        df_bs["coef_var_cce"].values,
+        df_bs["coef_var_nll"].values,
+        "CCE",
+        "NLL",
+        r"CCE vs NLL Coeff. of Variation ($\frac{\sigma}{\mu}$) "
+        f"- p value: {p_value_cce_nll:.3f}",
+        "./artifacts/cce_nll_kde.pdf",
+    )
+
+    print("Overall: ")
+    print(f"Mean CCE: {df_bs['coef_var_cce'].mean():.3f}")
+    print(f"Mean ECE: {df_bs['coef_var_ece'].mean():.3f}")
+    print(
+        f"CCE vs ECE p-value (two-sided): {p_value_cce_ece:.3f}",
+    )
+    print(
+        f"CCE vs ECE p-value (greater): {p_value_cce_ece_greater:.3f}",
+    )
 
     for g, df_g in df_bs.groupby("Dataset"):
 
